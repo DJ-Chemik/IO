a8615109209beb4f898b76e0d6be056302a2cd7e
jansmi <janek.smielowski@gmail.com>
2019-11-04 22:06:55 +0100

    Init Bootstrap Builder

---
+++ src/main/java/pl/poznan/put/bootstrapbuilder/app/BootstrapBuilderApplication.java
@@ -0,0 +1,13 @@
+package pl.poznan.put.bootstrapbuilder.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootstrapBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootstrapBuilderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/poznan/put/bootstrapbuilder/BootstrapBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.poznan.put.bootstrapbuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootstrapBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
