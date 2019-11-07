173c1c1fd48af34d30da76508c7d477955e5f0df
jansmi <janek.smielowski@gmail.com>
2019-11-08 00:47:46 +0100

    Declare GET method

--- src/main/java/pl/poznan/put/bootstrapbuilder/rest/BootstrapBuilderController.java
+++ src/main/java/pl/poznan/put/bootstrapbuilder/rest/BootstrapBuilderController.java
@@ -1,8 +1,17 @@
 package pl.poznan.put.bootstrapbuilder.rest;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BootstrapBuilderController {
+
+    @RequestMapping(value = "/bootstrap", method = RequestMethod.GET)
+    public ResponseEntity<Object> getBootstrap() {
+        // TODO: implement
+        return null;
+    }
 
 }
