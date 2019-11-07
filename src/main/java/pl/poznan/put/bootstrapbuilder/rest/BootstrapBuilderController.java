9f410444be19b5dc63672a580ff272f20649f8bb
jansmi <janek.smielowski@gmail.com>
2019-11-08 00:52:30 +0100

    Declare POST method

--- src/main/java/pl/poznan/put/bootstrapbuilder/rest/BootstrapBuilderController.java
+++ src/main/java/pl/poznan/put/bootstrapbuilder/rest/BootstrapBuilderController.java
@@ -1,6 +1,7 @@
 package pl.poznan.put.bootstrapbuilder.rest;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +11,13 @@ public class BootstrapBuilderController {
 
     @RequestMapping(value = "/bootstrap", method = RequestMethod.GET)
     public ResponseEntity<Object> getBootstrap() {
-        // TODO: implement
+        // TODO: implement method
+        return null;
+    }
+
+    @RequestMapping(value = "/bootstrap", method = RequestMethod.POST)
+    public ResponseEntity<Object> setBootstrap(@RequestBody BootstrapBuilder bootstrap) {
+        // TODO: implement method & create BootstrapBuilder ^^^^^^^^^^^^^^^^
         return null;
     }
 
