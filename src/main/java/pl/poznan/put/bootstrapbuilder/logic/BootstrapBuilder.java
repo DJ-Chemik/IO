eafff769a45b087ed54557326634ccb3524280b1
jansmi <janek.smielowski@gmail.com>
2019-11-09 20:49:04 +0100

    BootstrapBuilder fields declaration

--- src/main/java/pl/poznan/put/bootstrapbuilder/logic/BootstrapBuilder.java
+++ src/main/java/pl/poznan/put/bootstrapbuilder/logic/BootstrapBuilder.java
@@ -2,4 +2,11 @@ package pl.poznan.put.bootstrapbuilder.logic;
 
 public class BootstrapBuilder {
 
+    // fields declaration
+    private boolean header;
+    private boolean footer;
+    private String title, type, description, image;
+    private boolean headerVersion;
+    private int seoVersion;
+
 }
