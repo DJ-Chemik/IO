129c211a37ea01f0f00b16aea9789b00c14d6374
jansmi <janek.smielowski@gmail.com>
2019-11-18 19:52:59 +0100

    BootstrapBuilderController RequestMapping annotation updated & functions declaration rebuilt

--- src/main/java/pl/poznan/put/bootstrapbuilder/rest/BootstrapBuilderController.java
+++ src/main/java/pl/poznan/put/bootstrapbuilder/rest/BootstrapBuilderController.java
@@ -10,14 +10,14 @@ import pl.poznan.put.bootstrapbuilder.logic.BootstrapBuilder;
 @RestController
 public class BootstrapBuilderController {
 
-    @RequestMapping(value = "/bootstrap", method = RequestMethod.GET)
-    public ResponseEntity<Object> getBootstrap() {
+    @RequestMapping(value = "/bootstrap", method = RequestMethod.GET, produces = "application/json")
+    public String getViaParams() {
         // TODO: implement method
         return null;
     }
 
-    @RequestMapping(value = "/bootstrap", method = RequestMethod.POST)
-    public ResponseEntity<Object> setBootstrap(@RequestBody BootstrapBuilder bootstrap) {
+    @RequestMapping(value = "/bootstrap", method = RequestMethod.POST, produces = "application/json")
+    public String getViaBody(@RequestBody BootstrapBuilder bootstrap) {
         // TODO: implement method
         return null;
     }
