3f50bb20abe7e08629dcfd75079e4f19318f5788
jansmi <janek.smielowski@gmail.com>
2019-12-01 22:52:55 +0100

    Fixed test for Travis CI

--- src/test/java/pl/poznan/put/bootstrapbuilder/BootstrapBuilderApplicationTests.java
+++ src/test/java/pl/poznan/put/bootstrapbuilder/BootstrapBuilderApplicationTests.java
@@ -2,8 +2,9 @@ package pl.poznan.put.bootstrapbuilder;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import pl.poznan.put.bootstrapbuilder.app.BootstrapBuilderApplication;
 
-@SpringBootTest
+@SpringBootTest(classes = BootstrapBuilderApplication.class)
 class BootstrapBuilderApplicationTests {
 
 	@Test
