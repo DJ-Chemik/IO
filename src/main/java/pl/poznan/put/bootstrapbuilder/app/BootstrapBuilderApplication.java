dfefee262e6e0b5246a214376df50d1cdb5e0f1f
jansmi <janek.smielowski@gmail.com>
2019-12-01 19:29:54 +0100

    Javadoc for BootstrapBuilderApplication ready

--- src/main/java/pl/poznan/put/bootstrapbuilder/app/BootstrapBuilderApplication.java
+++ src/main/java/pl/poznan/put/bootstrapbuilder/app/BootstrapBuilderApplication.java
@@ -4,10 +4,21 @@ import com.vaadin.flow.spring.annotation.EnableVaadin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main BootstrapBuilder class, responsible for connecting Spring Boot and Vaadin
+ *
+ * @author Jan Śmiełowski, Mikołaj Szymczak
+ * @version 1.1
+ */
 @EnableVaadin({"pl.poznan.put.bootstrapbuilder.GUI"})
 @SpringBootApplication
 public class BootstrapBuilderApplication {
 
+    /**
+     * Main method, connecting Spring to application
+     *
+     * @param args parameters for the program (empty for normal boot)
+     */
 	public static void main(String[] args) {
 		SpringApplication.run(BootstrapBuilderApplication.class, args);
 	}
