8062fdf01f24afc203201e932a450d99a9bd0345
jansmi <janek.smielowski@gmail.com>
2019-12-01 20:27:26 +0100

    Javadoc for BootstrapBuilderController ready

--- src/main/java/pl/poznan/put/bootstrapbuilder/rest/BootstrapBuilderController.java
+++ src/main/java/pl/poznan/put/bootstrapbuilder/rest/BootstrapBuilderController.java
@@ -4,9 +4,29 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.poznan.put.bootstrapbuilder.logic.BootstrapBuilder;
 
+/**
+ * Main controller for dispatching REST requests
+ *
+ * @author Jan Śmiełowski
+ * @author Mikołaj Szymczak
+ * @version 2.0
+ */
 @RestController
 public class BootstrapBuilderController {
 
+    /**
+     * GET request mapping method
+     *
+     * @param header specifier for header
+     * @param footer specifier for footer
+     * @param title specifier for title
+     * @param type specifier for type
+     * @param description specifier for description
+     * @param image specifier for image
+     * @param headerVersion specifier for header version
+     * @param seoVersion specifier for SEO version
+     * @return Bootstrap code, formatted by params
+     */
     @RequestMapping(value = "/bootstrap", method = RequestMethod.GET, produces = "application/json")
     public static String getViaParams(@RequestParam(value = "header") boolean header, @RequestParam(value = "footer") boolean footer,
                                       @RequestParam(value = "title") String title,@RequestParam(value = "type") String type,
