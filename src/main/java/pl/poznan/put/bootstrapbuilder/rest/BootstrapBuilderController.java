7dae161ec8b8bcbe0814c1ff4474c88f9922969d
jansmi <janek.smielowski@gmail.com>
2019-12-01 22:24:19 +0100

    Fixed parameter values

--- src/main/java/pl/poznan/put/bootstrapbuilder/rest/BootstrapBuilderController.java
+++ src/main/java/pl/poznan/put/bootstrapbuilder/rest/BootstrapBuilderController.java
@@ -30,8 +30,8 @@ public class BootstrapBuilderController {
     @RequestMapping(value = "/bootstrap", method = RequestMethod.GET, produces = "application/json")
     public static String getViaParams(@RequestParam(value = "header") boolean header, @RequestParam(value = "footer") boolean footer,
                                       @RequestParam(value = "title") String title,@RequestParam(value = "type") String type,
-                                      @RequestParam(value = "desc") String description, @RequestParam(value = "image") String image,
-                                      @RequestParam(value = "headerversion") String headerVersion, @RequestParam(value = "seoversion") String seoVersion) {
+                                      @RequestParam(value = "description") String description, @RequestParam(value = "image") String image,
+                                      @RequestParam(value = "headerVersion") String headerVersion, @RequestParam(value = "seoVersion") String seoVersion) {
         BootstrapBuilder bootstrap = new BootstrapBuilder.Builder().header(header).footer(footer).title(title).type(type).description(description)
                 .image(image).headerVersion(headerVersion).seoVersion(seoVersion).build();
         return bootstrap.getBootstrap();
