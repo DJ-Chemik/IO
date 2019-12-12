905aebaa4cf0fcdaa28523b1f4772213c7dcba11
jansmi <janek.smielowski@gmail.com>
2019-12-12 21:06:03 +0100

    BootstrapBuilderController test initialized

---
+++ src/test/java/pl/poznan/put/bootstrapbuilder/rest/BootstrapBuilderControllerTest.java
@@ -0,0 +1,17 @@
+package pl.poznan.put.bootstrapbuilder.rest;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BootstrapBuilderControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
