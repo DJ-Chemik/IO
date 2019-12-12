e6b670c6cf70f91746a714d8bdeb03e651e23d8c
jansmi <janek.smielowski@gmail.com>
2019-12-12 21:05:02 +0100

    BootstrapBuilder test initialized

---
+++ src/test/java/pl/poznan/put/bootstrapbuilder/logic/BootstrapBuilderTest.java
@@ -0,0 +1,17 @@
+package pl.poznan.put.bootstrapbuilder.logic;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BootstrapBuilderTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
