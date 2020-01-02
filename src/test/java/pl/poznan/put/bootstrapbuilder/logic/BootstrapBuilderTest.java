7f0f69dbf8fa0f2c781bdc9dc085d058e87c6db3
jansmi <janek.smielowski@gmail.com>
2020-01-02 21:04:32 +0100

    Constructors tests implemented

--- src/test/java/pl/poznan/put/bootstrapbuilder/logic/BootstrapBuilderTest.java
+++ src/test/java/pl/poznan/put/bootstrapbuilder/logic/BootstrapBuilderTest.java
@@ -2,6 +2,7 @@ package pl.poznan.put.bootstrapbuilder.logic;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,4 +15,29 @@ class BootstrapBuilderTest {
     @AfterEach
     void tearDown() {
     }
+
+    @Test
+    void testBasicConstruction() {
+        BootstrapBuilder bootstrapBuilder = new BootstrapBuilder(true, true, "Title", "type",
+                "description", "image.jpg", "static", "og");
+        assertEquals("Title", bootstrapBuilder.getTitle());
+        assertEquals("static", bootstrapBuilder.isHeader() ? bootstrapBuilder.getHeaderVersion() : null);
+    }
+
+    @Test
+    void testBuilderConstruction() {
+        BootstrapBuilder bootstrapBuilder = new BootstrapBuilder.Builder()
+                .header(true)
+                .footer(true)
+                .title("Title")
+                .type("type")
+                .description("description")
+                .image("image.jpg")
+                .headerVersion("fixed")
+                .seoVersion("twitter")
+                .build();
+        assertEquals("Title", bootstrapBuilder.getTitle());
+        assertEquals("fixed", bootstrapBuilder.isHeader() ? bootstrapBuilder.getHeaderVersion() : null);
+    }
+
 }
