d55844fe49cb64f579cf4b3a3278a821361a9c10
jansmi <janek.smielowski@gmail.com>
2020-01-02 21:40:02 +0100

    Tests continued

--- src/test/java/pl/poznan/put/bootstrapbuilder/logic/BootstrapBuilderTest.java
+++ src/test/java/pl/poznan/put/bootstrapbuilder/logic/BootstrapBuilderTest.java
@@ -36,8 +36,53 @@ class BootstrapBuilderTest {
                 .headerVersion("fixed")
                 .seoVersion("twitter")
                 .build();
+
         assertEquals("Title", bootstrapBuilder.getTitle());
         assertEquals("fixed", bootstrapBuilder.isHeader() ? bootstrapBuilder.getHeaderVersion() : null);
+    }
+
+    @Test
+    void testSomeSetters() {
+        BootstrapBuilder bootstrapBuilder = new BootstrapBuilder(true, true, "Title", "type",
+                "description", "image.jpg", "static", "og");
+        assertTrue(bootstrapBuilder.isFooter());
+        assertEquals("description", bootstrapBuilder.getDescription());
+        assertEquals("static", bootstrapBuilder.isHeader() ? bootstrapBuilder.getHeaderVersion() : null);
+
+        bootstrapBuilder.setFooter(false);
+        bootstrapBuilder.setDescription("modified");
+        bootstrapBuilder.setHeaderVersion("fixed");
+
+        assertFalse(bootstrapBuilder.isFooter());
+        assertEquals("modified", bootstrapBuilder.getDescription());
+        assertEquals("fixed", bootstrapBuilder.isHeader() ? bootstrapBuilder.getHeaderVersion() : null);
+    }
+
+    @Test
+    void testBootstrap() {
+        BootstrapBuilder bootstrapBuilder = new BootstrapBuilder(true, true, "Title", "type",
+                "description", "image.jpg", "static", "og");
+        String bootstrap = bootstrapBuilder.getBootstrap();
+
+        assertTrue(bootstrap.contains("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">"));
+        assertTrue(bootstrap.contains("<meta name=\"og:title\" content=\"Title\">"));
+        assertTrue(bootstrap.contains("<nav class=\"navbar\">"));
+    }
+
+    @Test
+    void testUnknownHeaderVersion() {
+        BootstrapBuilder bootstrapBuilder = new BootstrapBuilder(true, true, "Title", "type",
+                "description", "image.jpg", "invalid", "og");
+        String bootstrap = bootstrapBuilder.getBootstrap();
+        // expecting static navbar
+        assertTrue(bootstrap.contains("<nav class=\"navbar\">"));
+    }
+
+    @Test
+    void testNullHeaderVersion() {
+        BootstrapBuilder bootstrapBuilder = new BootstrapBuilder(true, true, "Title", "type",
+                "description", "image.jpg", null, "og");
+        assertThrows(NullPointerException.class, bootstrapBuilder::getBootstrap);
     }
 
 }
