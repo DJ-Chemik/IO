e0bd7093a2a317b31eb047962ccf7b1b9a546d8b
jansmi <janek.smielowski@gmail.com>
2020-01-04 13:33:06 +0100

    Implementation of AbstractTest class for Spring web application deploy in tests

---
+++ src/test/java/pl/poznan/put/bootstrapbuilder/BootstrapBuilderAbstractTest.java
@@ -0,0 +1,38 @@
+package pl.poznan.put.bootstrapbuilder;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import pl.poznan.put.bootstrapbuilder.app.BootstrapBuilderApplication;
+
+import java.io.IOException;
+
+@SpringBootTest(classes = BootstrapBuilderApplication.class)
+@WebAppConfiguration
+public class BootstrapBuilderAbstractTest {
+    protected MockMvc mvc;
+    @Autowired
+    WebApplicationContext context;
+
+    protected void setUp() {
+        mvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
+
+    protected String mapToJson(Object obj) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.writeValueAsString(obj);
+    }
+    protected <T> T mapFromJson(String json, Class<T> clazz)
+            throws JsonParseException, JsonMappingException, IOException {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(json, clazz);
+    }
+}
