94be766a1c9f88e161d6903c81ca56f52cd54ed9
jansmi <janek.smielowski@gmail.com>
2020-01-04 15:29:38 +0100

    Implemented controller tests for GET and POST

--- src/test/java/pl/poznan/put/bootstrapbuilder/rest/BootstrapBuilderControllerTest.java
+++ src/test/java/pl/poznan/put/bootstrapbuilder/rest/BootstrapBuilderControllerTest.java
@@ -1,17 +1,48 @@
 package pl.poznan.put.bootstrapbuilder.rest;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import pl.poznan.put.bootstrapbuilder.BootstrapBuilderAbstractTest;
+import pl.poznan.put.bootstrapbuilder.logic.BootstrapBuilder;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
 
-class BootstrapBuilderControllerTest {
-
-    @BeforeEach
-    void setUp() {
+public class BootstrapBuilderControllerTest extends BootstrapBuilderAbstractTest {
+    @Override
+    @Before
+    public void setUp() {
+        super.setUp();
     }
 
-    @AfterEach
-    void tearDown() {
+    @Test
+    public void testGetRequest() throws Exception {
+        String uri = "/bootstrap?header=true&footer=true&title=Title&type=Type&description=desc&image=img.jpg&headerVersion=static&seoVersion=og";
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri).accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+        int status = mvcResult.getResponse().getStatus();
+        assertEquals(200, status);
+
+        String content = mvcResult.getResponse().getContentAsString();
+        String expected = new BootstrapBuilder(true, true, "Title", "Type",
+                "desc", "img.jpg", "static", "og").getBootstrap();
+        assertEquals(expected, content);
     }
+
+    @Test
+    public void testPostRequest() throws Exception {
+        BootstrapBuilder bootstrap = new BootstrapBuilder(true, true, "Title", "Type",
+                "desc", "img.jpg", "static", "og");
+        String json = super.mapToJson(bootstrap);
+
+        String uri = "/bootstrap";
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post(uri).contentType(MediaType.APPLICATION_JSON_VALUE).content(json)).andReturn();
+        int status = mvcResult.getResponse().getStatus();
+        assertEquals(200, status);
+
+        String content = mvcResult.getResponse().getContentAsString();
+        assertEquals(bootstrap.getBootstrap(), content);
+    }
+
 }
